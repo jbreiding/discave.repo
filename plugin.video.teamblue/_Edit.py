eafa0f110625e1107559de99f73bde05f28cc57f
discave <discavehb@live.com>
2016-09-14 15:54:56 +0100

    f4mproxy up

--- plugin.video.teamblue/_Edit.py
+++
@@ -1,5 +0,0 @@
-import xbmcaddon
-import base64
-
-MainBase = 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1RlYW1ibHVlMTk2NS9NRU5VUy9tYXN0ZXIvTUVOVV9QUklOQ0lQQUw='.decode('base64')
-addon = xbmcaddon.Addon('plugin.video.teamblue')
