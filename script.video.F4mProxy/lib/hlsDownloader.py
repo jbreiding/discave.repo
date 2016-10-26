b68c6cdf38dee770793587103688cbe9c46cbc6c
discave <discavehb@live.com>
2016-10-26 10:56:43 +0100

    addon ups

--- script.video.F4mProxy/lib/hlsDownloader.py
+++ script.video.F4mProxy/lib/hlsDownloader.py
@@ -136,7 +136,7 @@ class HLSDownloader():
         self.status='finished'
 
         
-def getUrl(url,timeout=15,returnres=False):
+def getUrl(url,timeout=15,returnres=False,stream=False):
     global cookieJar
     global clientHeader
     try:
@@ -156,9 +156,9 @@ def getUrl(url,timeout=15,returnres=False):
         #import random
         #headers['User-Agent'] =headers['User-Agent'] + str(int(random.random()*100000))
         if post:
-            req = session.post(url, headers = headers, data= post, proxies=proxies,verify=False,timeout=timeout)
+            req = session.post(url, headers = headers, data= post, proxies=proxies,verify=False,timeout=timeout,stream=stream)
         else:
-            req = session.get(url, headers=headers,proxies=proxies,verify=False ,timeout=timeout)
+            req = session.get(url, headers=headers,proxies=proxies,verify=False ,timeout=timeout,stream=stream)
 
         req.raise_for_status()
         if returnres: 
@@ -221,7 +221,7 @@ def download_chunks(URL, chunk_size=4096, enc=None):
     #conn=urllib2.urlopen(URL)
     #print 'starting download'
     
-    conn=getUrl(URL,timeout=6,returnres=True)
+    conn=getUrl(URL,timeout=6,returnres=True,stream=True)
     #while 1:
     if enc:
         if USEDec==1 :
@@ -463,6 +463,21 @@ def downloadInternal(url,file,maxbitrate=0,stopEvent=None):
     #dumpfile=open('c:\\temp\\myfile.mp4',"wb")
     variants = []
     variant = None
+     #url check if requires redirect
+    redirurl=url
+    try:
+        print 'going gor  ',url
+        res=getUrl(url,returnres=True )
+        print 'here ', res
+        if res.history: 
+            print 'history'
+            redirurl=res.url
+        res.close()
+        
+    except: traceback.print_exc()
+    print 'redirurl',redirurl
+    
+    
     for line in gen_m3u(url):
         if line.startswith('#EXT'):
             tag, attribs = parse_m3u_tag(line)
@@ -472,7 +487,7 @@ def downloadInternal(url,file,maxbitrate=0,stopEvent=None):
             variants.append((line, variant))
             variant = None
     if len(variants) == 1:
-        url = urlparse.urljoin(url, variants[0][0])
+        url = urlparse.urljoin(redirurl, variants[0][0])
     elif len(variants) >= 2:
         print "More than one variant of the stream was provided."
 
@@ -503,7 +518,7 @@ def downloadInternal(url,file,maxbitrate=0,stopEvent=None):
         if choice==-1: choice=0
         #choice = int(raw_input("Selection? "))
         print 'choose %d'%choice
-        url = urlparse.urljoin(url, variants[choice][0])
+        url = urlparse.urljoin(redirurl, variants[choice][0])
 
     #queue = Queue.Queue(1024) # 1024 blocks of 4K each ~ 4MB buffer
     control = ['go']
